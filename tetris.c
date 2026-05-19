2087ac73dfa6711d2f34179b880e66b5bf466d08
Jantovanne <jantovannedev@gmail.com>
2026-05-19 15:05:11 -0300

    Nivel-Novato-Concluido

--- tetris.c
+++ tetris.c
@@ -1,25 +1,134 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+#include <time.h>
 
 // Desafio Tetris Stack
 // Tema 3 - Integração de Fila e Pilha
 // Este código inicial serve como base para o desenvolvimento do sistema de controle de peças.
 // Use as instruções de cada nível para desenvolver o desafio.
 
+// estrutura para representar uma peça do Tetris.
+typedef struct {
+    char tipo;
+    int id;
+} Peca;
+
+// estrutura para representar a fila circular de peças futuras.
+typedef struct {
+    Peca peca[5];
+    int primeiro;
+    int ultimo;
+    int total;
+} Fila;
+
+int contador_id = 0; // Variável global para controle do ID das peças
+
+void inicializarFila(Fila* f) {
+    f->primeiro = 0;
+    f->ultimo = 0;
+    f->total = 0;
+}
+
+void gerarPeca(Peca* p) {
+    char tipos[] = {'I', 'O', 'T', 'L', 'Z'};
+    p->tipo = tipos[rand() % 5];
+    p->id = contador_id++;
+}
+
+// Função para adicionar uma peça à fila circular
+void enqueue(Fila* f, Peca p) {
+    if (f->total < 5) {
+        f->peca[f->ultimo] = p;
+        f->ultimo = (f->ultimo + 1) % 5;
+        f->total++;
+    } else {
+        printf("Fila cheia! Não é possível adicionar mais peças.\n");
+    }
+}
+
+// Função para exibir o conteúdo da fila circular
+void mostrarFila(Fila* f) {
+    printf("------------------------------\n");
+    printf("Fila de Peças Futuras:\n");
+    printf("------------------------------\n");
+    for (int i = 0; i < f->total; i++) {
+        int index = (f->primeiro + i) % 5;
+        printf("Peça ID: %d | Tipo: %c\n", f->peca[index].id, f->peca[index].tipo);
+        printf("------------------------------\n");
+    }
+}
+
+// Função para remover uma peça da fila circular
+void dequeue(Fila* f) {
+    if (f->total > 0) {
+        f->primeiro = (f->primeiro + 1) % 5;
+        f->total--;
+        mostrarFila(f);
+    } else {
+        printf("Fila vazia! Não há peças para remover.\n");
+    }
+}
+
+
+void menu(Fila* fila) {
+
+    int opcao;
+    do {
+    printf("----------\n");
+    printf("Menu:\n");
+    printf("----------\n");
+    printf("1 - Inserir peça na fila\n");
+    printf("2 - Jogar peça\n");
+    printf("0 - Sair\n");
+    printf("---------------------\n");
+    printf("Escolha uma opção: \n");
+    printf("----------------------\n");
+    scanf("%d", &opcao);
+    getchar(); // Limpar o buffer de entrada
+
+    switch (opcao)
+    {
+    case 1:
+    // Lógica para inserir peça na fila
+        Peca nova_peca;
+        gerarPeca(&nova_peca);
+        enqueue(fila, nova_peca);
+        mostrarFila(fila);
+        break;
+    case 2:
+        // Lógica para jogar peça
+        dequeue(fila);
+        break;
+    case 0:
+        printf("Saindo...\n");
+        break;
+    default:
+        printf("Opção inválida!\n");
+        break;
+    }
+
+    } while (opcao != 0);
+
+}
+
+
+
+
 int main() {
 
+    srand(time(NULL)); // Inicializa a semente para geração de peças aleatórias
+    setlocale(LC_ALL, ".UTF-8"); // Configura a localidade para UTF-8
     // 🧩 Nível Novato: Fila de Peças Futuras
-    //
-    // - Crie uma struct Peca com os campos: tipo (char) e id (int).
-    // - Implemente uma fila circular com capacidade para 5 peças.
-    // - Crie funções como inicializarFila(), enqueue(), dequeue(), filaCheia(), filaVazia().
-    // - Cada peça deve ser gerada automaticamente com um tipo aleatório e id sequencial.
-    // - Exiba a fila após cada ação com uma função mostrarFila().
-    // - Use um menu com opções como:
-    //      1 - Jogar peça (remover da frente)
-    //      0 - Sair
-    // - A cada remoção, insira uma nova peça ao final da fila.
+    Fila* fila = (Fila*) calloc(1, sizeof(Fila));
+    inicializarFila(fila);
+    
+    printf("================================\n");
+    printf("Bem vindo ao Tretis Stack!\n");
+    printf("================================\n");
 
-
+    menu(fila);
 
     // 🧠 Nível Aventureiro: Adição da Pilha de Reserva
     //
